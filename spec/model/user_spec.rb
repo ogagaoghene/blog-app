6808e0fa012a782dd00d8c0a9b82549f0568fa39
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-10-21 19:56:40 +0100

    Add unit test for Like model

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,32 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  subject do
+    User.new(name: 'Esi Ogagaoghene', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Nigeria.', posts_counters: 0)
+  end
 
   before { subject.save }
 
-  it 'name should be present' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  it 'name is not blank' do
+    expect(subject.name).to eq('Esi Ogagaoghene')
   end
 
-  it 'name should not be too short' do
-    subject.name = 'a'
-    expect(subject).to_not be_valid
+  it 'post_counter is an integer' do
+    Post.new do |post|
+      post.title = 'Hello World'
+      post.text = 'This is my first post'
+      post.user_id = subject.id
+      post.save
+    end
+    expect(subject.posts_counters).to be_an(Integer)
   end
 
-  it 'name should not be too long' do
-    subject.name = 'a' * 51
-    expect(subject).to_not be_valid
+  it 'blank name raises error' do
+    subject.name = ''
+    expect { subject.save! }.to raise_error(ActiveModel::StrictValidationFailed)
   end
 
-  it 'posts_counter should not be string' do
-    subject.posts_counter = 'abc'
-    expect(subject).to_not be_valid
-  end
-
-  it 'posts_counter should not be negative value' do
-    subject.posts_counter = -2
-    expect(subject).to_not be_valid
+  it 'post_counter thats not an integer raises error' do
+    subject.posts_counters = 'a'
+    expect { subject.save! }.to raise_error(ActiveRecord::RecordInvalid)
   end
 end
