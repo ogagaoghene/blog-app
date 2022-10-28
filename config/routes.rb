df4c145cdd1f4efc1e92bc07e426a43fa984352e
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-10-28 09:01:38 +0100

    Setup routes for users, posts, comments, and likes components or entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/new'
-  get 'likes/create'
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show]
-  end 
+  resources :users do
+    resources :posts
+  end
+  resources :posts do
+    resources :comments
+    resources :likes
+  end
   root to: 'users#index'
 end
