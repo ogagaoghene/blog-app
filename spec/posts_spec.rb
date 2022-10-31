924110bd2a863dc94dee16223e4eebf8b236f334
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-01 00:41:08 +0100

    Update spec and helpers

--- spec/posts_spec.rb
+++ spec/posts_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
-RSpec describe 'My Blog Post View', type: :feature do 
-  scenario 'index page' do 
-    visit 
-    expect(page).to have_content('Tom')
-  end 
+RSpec.describe 'My Blog Post View', type: :system do 
+  subject do
+    User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Nigeria.',
+             posts_counters: 0)
+  end
+
+  before { subject.save }
+
+  describe 'posts index page' do
+    before do
+      visit '/users/#{subject.id}/posts/'
+    end
+
+    it 'contains names of users' do
+      expect(page).to(have_content('Tom'))
+    end 
+  end
 end 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+require 'webdrivers'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require "capybara/rspec"
@@ -61,6 +62,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Capybara.configure do |config|
+    config.always_include_port = true
+  end
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
