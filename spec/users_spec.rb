98c42841fae3c348c0bc233febc3d7d654b27d36
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-02 00:30:34 +0100

    Add users_spec.rb

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -1,21 +1,27 @@
 require 'rails_helper'
-require 'webdrivers'
 
-RSpec.describe 'My Blog Post View', type: :system do 
-  subject do
-    User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Nigeria.',
-             posts_counters: 0)
-  end
-
-  before { subject.save }
-
-  describe 'users index page' do
-    before do
-      visit "/users/#{subject.id}/posts"
-    end
-
-    it 'contains names of users' do
-      expect(page).to(have_content('Tom'))
-    end 
-  end
+RSpec.describe 'Users', type: :system do 
+  scenario 'index page' do 
+    visit users_path
+    expect(page).to(have_content('Tom'))
+    expect(page).to(have_content('Lilly'))
+  end 
 end 
+# RSpec.describe 'My Blog Post View', type: :system do 
+#   subject do
+#     User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Nigeria.',
+#              posts_counters: 0)
+#   end
+
+#   before { subject.save }
+
+#   describe 'users index page' do
+#     before do
+#       visit "/users/#{subject.id}/posts"
+#     end
+
+#     it 'contains names of users' do
+#       expect(page).to(have_content('Tom'))
+#     end 
+#   end
+# end 
