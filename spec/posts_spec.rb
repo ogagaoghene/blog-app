f20b674d5c6a11e0d031f8bb4be4d4ebabf56375
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-02 08:34:38 +0100

    Update posts_spec.rb and users_spec.rb files

--- spec/posts_spec.rb
+++ spec/posts_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :system do 
-  scenario 'index page' do 
-    visit '/users/1/posts'
-    sleep(5)
-    img = page.find('img')
-    expect(img['src']).to(have_content('https://unsplash.com/photos/F_-0BxGuVvo'))
-  end 
-end 
+
 
 # RSpec.describe 'My Blog Post View', type: :system do 
 #   subject do
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do 
-  scenario 'index page' do 
-    visit users_path
-    expect(page).to(have_content('Esi Ogagaoghene'))
-  end 
-end 
+
 # RSpec.describe 'My Blog Post View', type: :system do 
 #   subject do
 #     User.new(id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Nigeria.',
