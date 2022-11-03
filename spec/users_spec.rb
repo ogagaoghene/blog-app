1b4fc1d76e86963db0d2e7d74222169e55ef2321
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-03 22:55:19 +0100

    Update users_spec.rb file

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -36,8 +36,25 @@ RSpec.describe 'Users', type: :system do
       visit users_path
       link = page.first('a')
       link.click
-      expect(page).to have_current_path('/users/1')
+      expect(page).to have_current_path('/users/2')
     end
+
+    it 'should render the number of posts for user -> Tom' do
+      visit 'users/1'
+      expect(page).to have_content('Number of posts: 2')
+    end
+
+    it 'should show number of posts for users' do 
+      visit 'users/'
+      posts_counter = page.all('.posts-counter')
+      expect(posts_counter[0]).to have_content('Number of posts: 1')
+      expect(posts_counter[1]).to have_content('Number of posts: 6')
+    end 
+
+    it 'should show number of posts for user -> Lilly' do 
+      visit 'users/2'
+      expect(page).to have_content('Number of posts: 1')
+    end 
 
     describe 'show page' do
       it 'should render the profile page of the user' do
@@ -48,17 +65,17 @@ RSpec.describe 'Users', type: :system do
 
       it 'should display the name of the user' do
         visit '/users/1'
-        expect(page).to have_content('Lilly')
+        expect(page).to have_content('Tom')
       end
 
       it 'should display the post counter of the user' do
         visit '/users/1'
-        expect(page).to have_content('Number of posts: 0')
+        expect(page).to have_content('Number of posts: 2')
       end
 
       it 'should display the bio of users' do
         visit '/users/1'
-        expect(page).to have_content('Teacher from Poland.')
+        expect(page).to have_content('Teacher from Mexico.')
       end
     end
   end
