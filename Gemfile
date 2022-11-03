9833ce38aed5b048d4197ee6a2d0e2ca313ae8f5
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-04 00:33:40 +0100

    Add config in environment.rb file

--- Gemfile
+++ Gemfile
@@ -48,4 +48,5 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'bullet'
 gem 'rubocop'
+gem 'devise'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = {host: 'localhost:3000'}
