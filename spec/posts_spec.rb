0164fba730f1e61a3de7df1a44c3dd44da7dbe4c
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-04 01:09:01 +0100

    Initial Commit

--- spec/posts_spec.rb
+++ spec/posts_spec.rb
@@ -1,100 +1,70 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :system do 
-  scenario 'index page' do 
+RSpec.describe 'Posts', type: :system do
+  before(:all) do
+    @lilly = User.create(name: 'Lilly',
+                         photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                         bio: 'Teacher from Poland.', posts_counters: 0)
+    @first_post = Post.create(author: @lilly, title: 'first post', text: 'This is my first post', likes_counter: 0,
+                              comments_counter: 0)
+    Comment.create(author: @lilly, post: @first_post, text: 'Your comment is on point')
+    Post.create(author: @lilly, title: 'second post', text: 'This is my second post', likes_counter: 0,
+                comments_counter: 0)
+  end
+
+  scenario 'page should have images of users' do
     visit '/users/1/posts'
-    sleep(5)
     img = page.find('img')
     expect(img['src']).to(have_content('https://unsplash.com/photos/F_-0BxGuVvo'))
-  end 
+  end
 
-  scenario 'display name of user' do
+  scenario 'page should display name of user' do
     visit '/users/1/posts'
-    sleep(5)
-    expect(page).to(have_content('Tom'))
+    expect(page).to(have_content('Lilly'))
   end
 
-  scenario 'display user posts counter' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page).to(have_content('Number of posts: 4'))
-  end
-
-  scenario 'display title of post' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page.to(have_content('enter title here'))
-  end
-
-  scenario 'display the body of post' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page).to(have_content('I love posting'))
-  end
-
-  scenario 'display the first comment of a post' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page).to have_content('I love comments')
-  end
-
-  scenario 'shows how many comments a post has' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page).to(have_content('Comments: 1'))
-  end
-
-  scenario 'shows how many comments a post has' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page).to(have_content('Comments: 1'))
-  end
-
-  scenario 'shows how many likes a post has' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page).to(have_content('Likes: 0'))
-  end
-
-  scenario 'redirects me to the post show page when clicking on a post' do
-    visit '/users/1/posts'
-    sleep(5)
-    expect(page).to(have_current_path('users/1/posts/1'))
-  end
-
-  scenario 'displays the posts title' do
-    visit '/users/1/posts/1'
-    expect(page).to have_content('new post')
-  end
-
-  scenario 'displays the posts author' do
+  scenario 'page should display the author of the posts' do
     visit '/users/1/posts/1'
     expect(page).to have_content('Lilly')
   end
 
-  scenario 'shows the number of comments' do
+  scenario 'page should show the number of comments' do
     visit '/users/1/posts/1'
     expect(page).to(have_content('Comments: 1'))
   end
 
-  scenario 'displays the number of likes' do
+  scenario 'page should display the number of likes' do
     visit '/users/1/posts/1'
-    expect(page).to(have_content('Likes: 0'))
+    expect(page).to(have_content('Likes: 1'))
   end
 
-  # it 'displays the body of posts' do
-  #   visit '/users/1/posts/1'
-  #   expect(page).to(have_content('I love posting'))
-  # end
+  scenario 'display number of posts' do
+    visit '/users/1/posts'
+    expect(page).to(have_content('Number of posts: 2'))
+  end
 
-  # it 'displays name of user that makes a comment' do
-  #   visit '/users/1/posts/1'
-  #   expect(page).to(have_content('Lilly'))
-  # end
+  scenario 'page should display number of comments' do
+    visit '/users/1/posts'
+    expect(page).to(have_content('Comments: 1'))
+  end
 
-  # it 'shows' do
-  #   visit '/users/1/posts/1'
-  #   expect(page).to(have_content('I love comments'))
-  # end
-  
-end 
+  scenario 'page should show the number of comments for a post' do
+    visit '/users/1/posts'
+    expect(page).to(have_content('Comments: 1'))
+  end
+
+  scenario 'page should display the number of likes for a post' do
+    visit '/users/1/posts'
+    expect(page).to(have_content('Likes: 1'))
+  end
+
+  scenario 'page should display the body of post' do
+    visit '/users/1/posts'
+    expect(page).to(have_content('This is my first post'))
+  end
+
+  scenario 'page should display the first comment of a post' do
+    visit '/users/1/posts'
+    expect(page).to have_content('Hi Ronaldo!')
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,16 @@
+# rubocop:disable Metrics/BlockLength
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'selenium-webdriver'
+# require 'selenium-webdriver'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rspec'
+require 'bullet'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,18 +32,27 @@ require 'capybara/rails'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
+  abort e.to_s.strip
 end
+
 RSpec.configure do |config|
+  Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
+  Capybara.javascript_driver = :selenium_chrome
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  Capybara.configure do |c|
+    c.run_server = false
+    c.app_host = 'http://127.0.0.1:3000'
+    c.server_port = 3000
+    c.default_host = 'http://127.0.0.1:3000'
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
-
+  config.include Capybara::DSL
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
@@ -76,6 +87,8 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :truncation
   end
 
+  # This block must be here, do not combine with the other `before(:each)` block.
+  # This makes it so Capybara can see the database.
   config.before(:each) do
     DatabaseCleaner.start
   end
@@ -83,6 +96,17 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
+  if Bullet.enable?
+    config.before(:each) do
+      Bullet.start_request
+    end
+
+    config.after(:each) do
+      Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
+  end
 end
-  
-Capybara.default_driver = :selenium
+
+# rubocop:enable Metrics/BlockLength
