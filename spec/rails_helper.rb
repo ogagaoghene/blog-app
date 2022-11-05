37bf8f3c70110b7c32a778ff6c875a7f8a76f033
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-05 08:20:24 +0100

    Update file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -90,23 +90,23 @@ RSpec.configure do |config|
   #   DatabaseCleaner.clean_with(:truncation)
   # end
 
-  # config.before(:each) do
-  #   DatabaseCleaner.strategy = :transaction
-  # end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
 
-  # config.before(:each, js: true) do
-  #   DatabaseCleaner.strategy = :truncation
-  # end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
-  # # This block must be here, do not combine with the other `before(:each)` block.
-  # # This makes it so Capybara can see the database.
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
+  # This block must be here, do not combine with the other `before(:each)` block.
+  # This makes it so Capybara can see the database.
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   if Bullet.enable?
     config.before(:each) do
