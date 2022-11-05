ece455376710b118c440c044250989f017f86b37
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-05 08:23:45 +0100

    Update file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -86,9 +86,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  # config.before(:suite) do
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
