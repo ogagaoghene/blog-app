5e51a6a9100687eb65cfcb526b2466da838f4992
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-07 10:56:34 +0100

    Initial commit of blog app in clean state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_193447) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_171318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
