61aca0522c46929fb5b14cb99414c6ce1b27c16b
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-15 22:19:15 +0100

    Add SMTP settings to production.rb and environment.rb files

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.perform_deliveries = true
-# ActionMailer::Base.raise_delivery_errors = true
-# ActionMailer::Base.smtp_settings = {
-#   address: 'smtp.gmail.com',
-#   port: 587,
-#   domain: ENV['gmail.com'],
-#   user_name: ENV['otipuebi@gmail.com'],
-#   password: ENV['plsoaibknjiolpji'],
-#   authentication: 'plain',
-#   :enable_starttls_auto => true
-#  }
-
