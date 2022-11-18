1894514f9ad5c8b546e9f7d3cc48dff40763d66d
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-18 09:56:53 +0100

    Update devise user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: false
   skip_before_action :verify_authenticity_token, only: [:create]
-  before_action :authenticate_login!
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def current_ability
-    @current_ability ||= Ability.new(current_login)
-  end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password password_confirmation])
   end
 
   rescue_from CanCan::AccessDenied do |exception|
