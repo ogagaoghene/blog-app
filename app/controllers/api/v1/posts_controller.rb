42559fc275eb6b9813efd04de0ac13aac556bbe1
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-18 16:06:17 +0100

    Update posts_controllers.rb endpoint

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::PostsController < ApplicationController
 
   def show
     @users = User.find(params[:user_id])
-    @posts = @user.posts.find(params[:id])
+    @posts = @users.posts.find(params[:id])
     respond_to do |format|
       format.json { render json: @posts }
     end
