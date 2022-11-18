0e6e049b03b073d3431a60cb0fb22cc642f1663e
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-18 18:14:53 +0100

    Update config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
+  devise_for :users,
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations'
+  }
   devise_scope  :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
